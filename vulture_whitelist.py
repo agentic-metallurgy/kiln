45424d35d7792841dba560c90a23c47a4afea1c3
Elon Demirok <elon.demirok@gmail.com>
2026-02-07 18:56:28 -0800

    fix: add vulture whitelist for __exit__ protocol parameters

---
+++ vulture_whitelist.py
@@ -0,0 +1,6 @@
+# Vulture whitelist - suppress false positives for required protocol parameters
+
+# Context manager __exit__ parameters (required by protocol)
+exc_type  # noqa
+exc_val  # noqa
+exc_tb  # noqa
